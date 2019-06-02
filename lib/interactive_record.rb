2f5ae7e5f40c627ca0f1f903842f3c513d88e604
David Stewart <davidstewart@gmail.com>
2019-06-02 14:56:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,12 @@ def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
   DB[:conn].execute(sql,name)
 end
-
+def self.find_by(hash)
+    value = hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+end
 
 end
  
